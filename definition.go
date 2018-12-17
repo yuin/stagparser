ea298cc277636071236d3e340accb95a4d1742e2
yuin <yuin@inforno.net>
2018-12-17 14:58:43 +0900

    update godoc

--- definition.go
+++ definition.go
@@ -1,8 +1,12 @@
 package stagparser
 
+// Definition is a struct tag value element
 type Definition interface {
+	// Name is a name of the definition
 	Name() string
+	// Attributes are attributes of the definition
 	Attributes() map[string]interface{}
+	// Attribute returns an attribute value and true if an attribute exists
 	Attribute(name string) (interface{}, bool)
 }
 
